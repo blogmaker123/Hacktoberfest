d3f64fe1c794eafa23db9744309fa90c4a719580
Anuj8100 <91829537+Anuj8100@users.noreply.github.com>
2021-10-03 20:51:05 +0530

    Create autoTyper.py

---
+++ autoTyper.py
@@ -0,0 +1,34 @@
+from pynput.keyboard import Key, Controller
+import time
+import os
+keyboard = Controller()
+while True:
+    n = input("[__] Enter Time After Which Typing Should Start: ")
+    try:
+    	n = int(n)
+    	break
+    except Exceptiona as e:
+    	print("[!] Please Enter Right Input: Integet!")
+    	continue
+for i in range(n):
+    print("[*] This app will start autotyping in",n - i,"second(s).",end='\r')
+    i-=1
+    time.sleep(1)
+lst = os.listdir()
+if "data.txt" not in lst:
+    print("[!] 'data.txt' not found! Creating file...")
+    with open("data.txt", "w") as file_w:
+        file_w.write("Hello World!\nWelcome To AutoTyper")
+        print("[*] Done!")
+
+print("[*] Autotyping has started. Please close this window to stop autotyping!")
+
+with open('data.txt', 'r') as file_r:
+    typeString = file_r.read()
+    
+for char in typeString:
+    if char == '\n':
+        keyboard.type("\n")    
+    else:
+        keyboard.type(char)    
+    time.sleep(0.03)
