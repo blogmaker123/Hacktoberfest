c253fb05abbcae2a1e8e921a43d07d4509e82715
Winner121353 <50233676+Winner121353@users.noreply.github.com>
2021-10-18 00:29:24 +0530

    Update number-guessing-game.py
    Added comments for the game

--- python files/number-guessing-game.py
+++ python files/number-guessing-game.py
@@ -1,11 +1,17 @@
+#Number Guessing Game
 import random
 print("Hi, welcome to the number guessing game!")
 print("pick a number between 1 and 50!)
+
 guess_taken = 0
-computer_guess=random.randint(1, 50)
+computer_guess = random.randint(1, 50) #Random integer from 1-50
 print("is it " number"? yes, higher, or lower")
 guess_taken += 1
+
+#Input Higher or Lower or Yes
 decision=input()
+      
+#Game Logic
 while decision =! "yes"
       if decision = "lower"
         new_computer_guess = random.randint(0, computer_guess)
