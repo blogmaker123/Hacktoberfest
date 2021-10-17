3390c89d0b0c5fc2d94cff696b39676d7feefb7c
rompulous <92483471+rompulous@users.noreply.github.com>
2021-10-17 11:04:21 -0700

    Create FahrenheitToCelsius.py

---
+++ python files/FahrenheitToCelsius.py
@@ -0,0 +1,8 @@
+# take input of Fahrenheit
+f = float(input("Temperature in Fahrenheit is: "))
+
+# calculate the celcius
+c = (f - 32) * (5/9)
+
+# print the celcius
+print("Temperature in Celcius is ", c)
